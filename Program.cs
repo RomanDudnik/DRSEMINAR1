a6ebb35cc261f236ed2eb3b3f2263e4286324240
«Roman <research777@mail.ru>
2022-11-12 16:51:44 +0300

    Задача4.v3

--- Program.cs
+++ Program.cs
@@ -86,6 +86,64 @@ else
     }
 }
 */
+//Вариант3
+/*
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 && num2 == num3)
+{
+    Console.WriteLine($"Все числа равны {num1} ");
+}
+else
+{
+
+    if(num1 == num2 && num1 > num3)
+    {
+        Console.WriteLine($"Два введенных числа равны и являются максимальным числом {num1} ");
+    }    
+    else
+    {
+
+        if(num1 > num2 && num1 == num3)
+        {
+            Console.WriteLine($"Два введенных числа равны и являются максимальным числом {num1} ");
+        }
+        else
+        {
+
+            if(num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine($"Число {num1} является максимальным ");
+            }
+            else
+            {
+
+                if(num2 == num3)
+                {
+                    Console.WriteLine($"Два введенных числа равны и являются максимальными {num2} ");
+                }
+                else
+                {
+
+                    if(num2 > num3)
+                    {
+                        Console.WriteLine($"Число {num2} является максимальным ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Число {num3} является максимальным ");
+                    }
+                }    
+            }
+        }
+    }      
+}
+*/
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 //является ли число чётным (делится ли оно на два без остатка).
 
