d55a90ce301cc9340747810646218f15ff946b4c
«Roman <research777@mail.ru>
2022-11-12 15:20:47 +0300

    Задача6

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
-/* Console.WriteLine("Введите первое число: ");
+/*
+Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -20,8 +20,8 @@ else
 {
     Console.WriteLine($"число {number2} является большим");
     Console.WriteLine($"число {number1} является меньшим");
-}*/
-
+}
+*/
 // Задача 4: Напишите программу, которая принимает на вход три числа и 
 // выдаёт максимальное из этих чисел.
 
@@ -29,6 +29,8 @@ else
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+//Вариант1:
+/*
 Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
@@ -58,15 +60,55 @@ else
         Console.WriteLine($"число {num3} является максимальным");
     }
 }
+*/
+//Вариант2:
+/*
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine($"число {num1} является максимальным");
+}
+else
+{
+    if (num2 > num3)
+    {
+        Console.WriteLine($"число {num2} является максимальным ");
+    }
+    else
+    {
+        Console.WriteLine($"число {num3} является максимальным ");
+    }
+}
+*/
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
 
 // 4 -> да
 // -3 -> нет
-// 7 -> нет
+// 7 -> нет*/
+/*
+Console.WriteLine("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+int current = (int)number;
 
+if (number / 2 > current / 2)
+{
+    Console.WriteLine($"Число {number} не является четным ");
+}
+else
+{
+    Console.WriteLine($"Число {number} является четным ");
+}
+*/
 // Задача 8: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
